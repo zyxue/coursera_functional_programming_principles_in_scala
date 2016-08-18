f62e86802a9109bcac428af67808dc2f091e7655
Zhuyi Xue <alfred532008@gmail.com>
2016-08-17 17:10:27 -0700

    implemented sum & max recursively

--- week1/example/src/main/scala/example/Lists.scala
+++ week1/example/src/main/scala/example/Lists.scala
@@ -23,7 +23,14 @@ object Lists {
    * @param xs A list of natural numbers
    * @return The sum of all elements in `xs`
    */
-    def sum(xs: List[Int]): Int = ???
+  def sum(xs: List[Int]): Int = {
+    if (xs.isEmpty) {
+      0
+    }
+    else {
+      xs.head + sum(xs.tail)
+    }
+  }
   
   /**
    * This method returns the largest element in a list of integers. If the
@@ -38,5 +45,17 @@ object Lists {
    * @return The largest element in `xs`
    * @throws java.util.NoSuchElementException if `xs` is an empty list
    */
-    def max(xs: List[Int]): Int = ???
+  def max(xs: List[Int]): Int = {
+    if (xs.isEmpty) {
+      throw new NoSuchElementException
+    }
+    else if (xs.length == 1) {
+      xs.head
+    }
+    else {
+      val m = max(xs.tail)
+      if (xs.head > m) xs.head
+      else m
+    }
   }
+}
