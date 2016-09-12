3f354e1fc4bb952b9cdd9b74ce45a4a192eabf9c
Zhuyi Xue <alfred532008@gmail.com>
2016-09-12 13:11:40 -0700

    implemented mergeCodeTables & quickEncode

--- homework/week4-types-and-pattern-matching/patmat/src/main/scala/patmat/Huffman.scala
+++ homework/week4-types-and-pattern-matching/patmat/src/main/scala/patmat/Huffman.scala
@@ -325,7 +325,9 @@ object Huffman {
     * use it in the `convert` method above, this merge method might also do some transformations
     * on the two parameter code tables.
     */
-  def mergeCodeTables(a: CodeTable, b: CodeTable): CodeTable = ???
+  def mergeCodeTables(a: CodeTable, b: CodeTable): CodeTable = {
+    a ::: b
+  }
 
   /**
     * This function encodes `text` according to the code tree `tree`.
@@ -333,5 +335,12 @@ object Huffman {
     * To speed up the encoding process, it first converts the code tree to a code table
     * and then uses it to perform the actual encoding.
     */
-  def quickEncode(tree: CodeTree)(text: List[Char]): List[Bit] = ???
+  def quickEncode(tree: CodeTree)(text: List[Char]): List[Bit] = {
+    var res = List[Bit]()
+    val table = convert(tree)
+    for (c <- text) {
+      res = res ::: codeBits(table)(c)
+    }
+    res
+  }
 }
